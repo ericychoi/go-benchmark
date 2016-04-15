99300cfb292fc23097b2a7830780c683d0ca6b25
Eric Choi <eric.choi@sendgrid.com>
2016-04-14 23:23:48 -0700

    add benchmarking on initialization of maps

--- map_insert_test.go
+++ map_insert_test.go
@@ -30,3 +30,18 @@ func BenchmarkMapStaticInsert(b *testing.B) {
 		structMap[a] = struct{}{}
 	}
 }
+
+func benchmarkMapInit(size int, b *testing.B) {
+	var structMap map[string]struct{}
+	for i := 0; i < b.N; i++ {
+		structMap = make(map[string]struct{}, size)
+	}
+	structMap["a"] = struct{}{}
+}
+
+func BenchmarkMapInit10(b *testing.B)      { benchmarkMapInit(10, b) }
+func BenchmarkMapInit100(b *testing.B)     { benchmarkMapInit(100, b) }
+func BenchmarkMapInit1000(b *testing.B)    { benchmarkMapInit(1000, b) }
+func BenchmarkMapInit10000(b *testing.B)   { benchmarkMapInit(10000, b) }
+func BenchmarkMapInit100000(b *testing.B)  { benchmarkMapInit(100000, b) }
+func BenchmarkMapInit1000000(b *testing.B) { benchmarkMapInit(1000000, b) }
